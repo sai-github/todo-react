3959dfd056a2bbe7e25cb6ed77bef5ae219d4892
sai-github <saiediitm@gmail.com>
2020-11-01 02:14:56 +0530

    feat: 🎸 add new todo to list

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -7,9 +7,12 @@ class Todo extends React.Component {
   constructor() {
     super();
     this.state = {
+      newTaskText: "",
       todos: taskData,
     };
     this.handleChange = this.handleChange.bind(this);
+    this.updateNewText = this.updateNewText.bind(this);
+    this.onAddNewText = this.onAddNewText.bind(this);
   }
 
   handleChange(id) {
@@ -30,6 +33,35 @@ class Todo extends React.Component {
     });
   }
 
+  updateNewText($event) {
+    const { name, value } = $event.target;
+    this.setState({ [name]: value });
+  }
+
+  uuidv4() {
+    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+      (
+        c ^
+        (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+      ).toString(16)
+    );
+  }
+
+  onAddNewText() {
+    this.setState((prevState) => {
+      const updatedToDos = [
+        ...prevState.todos,
+        { id: this.uuidv4(), task: this.state.newTaskText, status: false },
+      ];
+
+      return {
+        todos: updatedToDos,
+      };
+    });
+
+    this.setState({ newTaskText: "" });
+  }
+
   render() {
     const containerStyle = {
       display: "flex",
@@ -46,6 +78,20 @@ class Todo extends React.Component {
 
     return (
       <div className="card" style={containerStyle}>
+        <div className="new-task-container">
+          <input
+            name="newTaskText"
+            placeholder="Add task"
+            value={this.state.newTaskText}
+            onChange={this.updateNewText}
+          />
+          <button onClick={this.onAddNewText}>
+            <span role="img" aria-label="Add new task">
+              ➕
+            </span>
+          </button>
+        </div>
+
         {taskItemComponents}
       </div>
     );
